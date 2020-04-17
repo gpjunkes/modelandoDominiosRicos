807297738aabcb9034539780e96c9b45f783a6a8
guilhermejunkes <guilhermepjunkes@gmail.com>
2020-04-16 21:25:40 -0300

    Primeira parte - Modelando domínios ricos

---
+++ PaymentContext.Domain/Entities/PayPalPayment.cs
@@ -0,0 +1,32 @@
+using System;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class PayPalPayment : Payment
+    {
+        public PayPalPayment(
+            string transactionCode,
+            DateTime paidDate,
+            DateTime expireDate, 
+            decimal total, 
+            decimal totalPaid, 
+            Document document, 
+            string payer, 
+            Address adress, 
+            Email email) : base(
+                paidDate, 
+                expireDate, 
+                total, 
+                totalPaid, 
+                document, 
+                payer, 
+                adress, 
+                email)
+        {
+            TransactionCode = transactionCode;
+        }
+
+        public string TransactionCode { get; private set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var name = new Name("Teste", "Teste");
+            foreach(var not in name.Notifications){
+                //not.Message;
+            }
+        }
+    }
+}
